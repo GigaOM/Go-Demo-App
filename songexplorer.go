71c8007571e6f62fa3a4b3497b7a7e3f64c9dfe4
Michael Levan <mlevan1992@gmail.com>
2021-12-24 13:13:58 -0500

    songexplorer

--- songexplorer.go
+++ songexplorer.go
@@ -2,28 +2,31 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/jinzhu/gorm"
 )
 
 func main() {
-	sqlPath := "root:root@/todolist?charset=utf8&parseTime=True&loc=Local"
-	db, err := gorm.Open("mysql", sqlPath)
+	// sqlPath := "root:root@/todolist?charset=utf8&parseTime=True&loc=Local"
+	// db, err := gorm.Open("mysql", sqlPath)
 
-	if err != nil {
-		log.Fatal("Connection to database failed")
-	}
+	// if err != nil {
+	// 	log.Fatal("Connection to database failed")
+	// }
 
-	defer db.Close()
+	// defer db.Close()
 
 	router := mux.NewRouter()
 	router.HandleFunc("/createsong", songExplorerCreate).Methods("POST")
 	router.HandleFunc("/updatesong", songExplorerUpdate).Methods("POST")
 	router.HandleFunc("/deletesong", songExplorerDelete).Methods("POST")
+	router.HandleFunc("/getsong", songExplorerGet).Methods("GET")
 	http.ListenAndServe(":8000", router)
+}
+
+func songExplorerGet(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("WIP")
 }
 
 func songExplorerCreate(w http.ResponseWriter, r *http.Request) {
