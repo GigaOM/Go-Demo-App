4ae9e2f43d6c7ca6d026d9be71ea84dacee5afb2
Michael Levan <mlevan1992@gmail.com>
2021-12-26 11:18:44 -0500

    DB connection

--- songexplorer.go
+++ songexplorer.go
@@ -10,7 +10,7 @@ import (
 )
 
 // Database Connection
-var dsn = "mike@benchmarktest01:Password1@tcp(benchmarktest01.mysql.database.azure.com:3306)/benchmarktest01?charset=utf8mb4 parseTime=True&loc=Local"
+var dsn = "mike@benchmarktest01:Password1@tcp(benchmarktest01.mysql.database.azure.com:3306)/benchmarks?charset=utf8mb4"
 var db, _ = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 type SongExplorerModel struct {
