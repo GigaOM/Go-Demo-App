02cb6c0298e05fcc2df99a395c6293b242034bce
Michael Levan <mlevan1992@gmail.com>
2021-12-30 07:23:20 -0500

    mysql commands

--- mysql_table.sql
+++ mysql_table.sql
@@ -1,2 +1,4 @@
+mysql --host=benchmarktest01.mysql.database.azure.com --user=mike -p
 create database song_explorer_models;
-create table song_explorer_models (Year VARCHAR(100), song_name VARCHAR(100), ID VARCHAR(100), created_at VARCHAR(100)
+use song_explorer_models;
+create table song_explorer_models (Year VARCHAR(100), song_name VARCHAR(100), ID VARCHAR(100), created_at VARCHAR(100);
